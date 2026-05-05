89484b0d997f959aa6d423d6a69d9fdc04b329a7
Yogendra <yk1082420@gmail.com>
2026-05-05 12:46:35 +0530

    fix: remove returnRoutes, set node version

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
 
 let socket = null;
 
